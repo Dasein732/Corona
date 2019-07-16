fedeb6d69fb00a9340328fa4351453147cde7c9b
todor <milostodorovic@protonmail.com>
2019-07-16 16:30:49 +0200

    Add project files.

---
+++ program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace program
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
