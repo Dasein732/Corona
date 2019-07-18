2e125d1d8c0a19955f370820f877ac988eba9850
todor <milostodorovic@protonmail.com>
2019-07-18 15:11:01 +0200

    add ray struct

---
+++ program/Ray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Program
+{
+    public struct Ray
+    {
+        public Vector4 Origin;
+        public Vector4 Direction;
+
+        public Vector4 Position(float tDistance)
+        {
+            return Position(this, tDistance);
+        }
+
+        public static Vector4 Position(Ray ray, float tDistance)
+        {
+            return ray.Origin + ray.Direction * tDistance;
+        }
+    }
+}
